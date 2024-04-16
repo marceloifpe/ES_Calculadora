314e6834303365099030900a650f25b211971450
Marcelo Augusto <marcelo513.ma@gmail.com>
2024-04-16 20:10:52 -0300

    Teste_soma_funcional

---
+++ test_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+from calculator import soma
+
+class TestSoma(unittest.TestCase):
+
+    def test_soma_posi(self):
+        self.assertEqual(soma(1, 2, 3, 4, 5), 15)
+
+    def test_soma_nega(self):
+        self.assertEqual(soma(-1, -2, -3, -4, -5), -15)
+
+    def test_soma_posi_nega(self):
+        self.assertEqual(soma(-1, 2, -3, 4, -5), -3)
+
+    def test_soma_zer(self):
+        self.assertEqual(soma(0, 1, 2, 3), 6)
+        self.assertEqual(soma(0, -1, -2, -3), -6)
+
+    def test_soma_rea(self):
+        self.assertAlmostEqual(soma(0.1, 0.2), 0.3, places=7)
+
+    def test_soma_num_uni(self):
+        self.assertEqual(soma(7), 7)
+
+if __name__ == '__main__':
+    unittest.main()
+

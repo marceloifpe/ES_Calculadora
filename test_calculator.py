2dc5a7b63727babe5c4fe4d17110615fc3e82077
Marcelo Augusto <marcelo513.ma@gmail.com>
2024-04-19 10:04:23 -0300

    Implementando_Divisão
    com Número positivos, negativos, por zero, numero único e números reais

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 import unittest
-from calculator import soma
+from calculator import soma, divisao
 
-class TestSoma(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
 
     def test_soma_posi(self):
         self.assertEqual(soma(1, 2, 3, 4, 5), 15)
@@ -22,6 +22,15 @@ class TestSoma(unittest.TestCase):
     def test_soma_num_uni(self):
         self.assertEqual(soma(7), 7)
 
+    def test_divisao_posi(self):
+        self.assertEqual(divisao(10, 2), 5)
+
+    def test_divisao_nega(self):
+        self.assertEqual(divisao(-10, 2), -5)
+
+    def test_divisao_zero(self):
+        with self.assertRaises(ValueError):
+            divisao(10, 0)
+
 if __name__ == '__main__':
     unittest.main()
-

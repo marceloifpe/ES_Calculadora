88c2d255b5f9cb6f1c70054ee603bb5b02c86376
Pedro Henrique Lima Pereira <100004751+PedroHLP25@users.noreply.github.com>
2024-04-19 22:13:37 -0300

    Juntando vários testes de todas as operações em um
    Mais testes juntando todos em um geral

--- testes.py
+++ testes.py
@@ -3,8 +3,10 @@ from main import Calculadora
 
 class TestCalculadora(unittest.TestCase):
     def setUp(self):
-             self.calculadora = Calculadora(10, 5)
-      
+       
+        self.calculadora = Calculadora(10, 5)
+
+     # Testes Soma
     def test_somar_positivo_positivo(self):
         self.assertEqual(self.calculadora.somar(), 15)
 
@@ -28,6 +30,32 @@ class TestCalculadora(unittest.TestCase):
         self.calculadora = Calculadora(13, 7)
         self.assertEqual(self.calculadora.somar(), 20)
 
+        # Testes Subtração
+
+    def test_subtrair_positivo_positivo(self):
+        self.assertEqual(self.calculadora.subtrair(), 5)
+
+    def test_subtrair_negativo_negativo(self):
+        c = Calculadora(-10, -5)
+        self.assertEqual(c.subtrair(), -5)
+
+    def test_subtrair_positivo_negativo(self):
+        c = Calculadora(10, -5)
+        self.assertEqual(c.subtrair(), 15)
+
+    def test_subtrair_negativo_positivo(self):
+        c = Calculadora(-10, 5)
+        self.assertEqual(c.subtrair(), -15)
+
+    def test_subtrair_neutro(self):
+        c = Calculadora(0, 5)
+        self.assertEqual(c.subtrair(), -5)
+
+    def test_subtrair_positivo_positivo_custom(self):
+        self.calculadora = Calculadora(13, 7)
+        self.assertEqual(self.calculadora.subtrair(), 6)
+
+        # Testes Multiplicação
 
     def test_multiplicar_positivo_positivo(self):
         self.assertEqual(self.calculadora.multiplicar(), 50)
@@ -52,11 +80,30 @@ class TestCalculadora(unittest.TestCase):
         self.calculadora = Calculadora(13, 7)
         self.assertEqual(self.calculadora.multiplicar(), 91)
 
+
+        # Testes Divisão
+    def test_dividir_positivo_positivo(self):
+        self.assertEqual(self.calculadora.dividir(), 2)
+
+    def test_dividir_negativo_negativo(self):
+        c = Calculadora(-10, -5)
+        self.assertEqual(c.dividir(), 2)
+
+    def test_dividir_positivo_negativo(self):
+        c = Calculadora(10, -5)
+        self.assertEqual(c.dividir(), -2)
+
+    def test_dividir_negativo_positivo(self):
+        c = Calculadora(-10, 5)
+        self.assertEqual(c.dividir(), -2)
+
+    def test_dividir_neutro(self):
+        c = Calculadora(0, 5)
+        self.assertEqual(c.dividir(), 0)
+
+    def test_dividir_positivo_positivo_custom(self):
+        self.calculadora = Calculadora(14, 7)
+        self.assertEqual(self.calculadora.dividir(), 2)
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
